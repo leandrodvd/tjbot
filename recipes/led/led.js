30326325cf93ff0d5dc6b47dd260f7e44ac998d1
Leandro David <leandrodvd@gmail.com>
2017-07-16 01:41:42 -0300

    update led.js

--- recipes/led/led.js
+++ recipes/led/led.js
@@ -5,29 +5,26 @@ var led_RED = new Gpio(17, 'out');
 var led_GREEN = new Gpio(27, 'out');
 var led_BLUE = new Gpio(22, 'out');
 
-led_RED.writeSync(1);
-sleep.sleep(20); 
-
 function setLED(r,g,b){
   console.log("set led to R:"+r+" G:"+g+" B:"+b);
   led_RED.writeSync(r);
   led_GREEN.writeSync(g);
   led_BLUE.writeSync(b);
 }
-while (true){
+while (1){
 
-  setLED(true,false,false);
+  setLED(1,0,0);
   sleep.sleep(2); // sleep for ten seconds
-  setLED(false,true,false);
+  setLED(0,1,0);
   sleep.sleep(2);
-  setLED(false,false,true);
+  setLED(0,0,1);
   sleep.sleep(2);
-  setLED(true,true,false);
+  setLED(1,1,0);
   sleep.sleep(2);
-  setLED(true,true,true);
+  setLED(1,1,1);
   sleep.sleep(2);
-  setLED(true,false,true);
+  setLED(1,0,1);
   sleep.sleep(2);
-  setLED(false,true,true);
+  setLED(0,1,1);
   sleep.sleep(2);
 }
