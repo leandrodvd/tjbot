18773e13d66999257ce11518e2fe9cdc097b4600
Leandro David <leandrodvd@gmail.com>
2017-07-16 01:23:06 -0300

    add led sample

---
+++ recipes/led/led.js
@@ -0,0 +1,28 @@
+var Gpio = require('onoff').Gpio;
+var sleep = require('sleep');
+
+var led_RED = new Gpio(17, 'out');
+var led_GREEN = new Gpio(27, 'out');
+var led_BLUE = new Gpio(22, 'out');
+
+function setLED(r,g,b){
+  led_RED.writeSync(r);
+  led_GREEN.writeSync(g);
+  led_BLUE.writeSync(b);
+}
+while (true){
+  setLED(true,false,false);
+  sleep.sleep(2); // sleep for ten seconds
+  setLED(false,true,false);
+  sleep.sleep(2);
+  setLED(false,false,true);
+  sleep.sleep(2);
+  setLED(true,true,false);
+  sleep.sleep(2);
+  setLED(true,true,true);
+  sleep.sleep(2);
+  setLED(true,false,true);
+  sleep.sleep(2);
+  setLED(false,true,true);
+  sleep.sleep(2);
+}
