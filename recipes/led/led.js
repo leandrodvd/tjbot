4ade8d39b9fbafacf9b6a264cf70afdf01b3dfdd
Leandro David <ldavid@br.ibm.com>
2017-07-16 01:37:48 -0300

    fix led output value

--- recipes/led/led.js
+++ recipes/led/led.js
@@ -5,12 +5,17 @@ var led_RED = new Gpio(17, 'out');
 var led_GREEN = new Gpio(27, 'out');
 var led_BLUE = new Gpio(22, 'out');
 
+led_RED.writeSync(1);
+sleep.sleep(20); 
+
 function setLED(r,g,b){
+  console.log("set led to R:"+r+" G:"+g+" B:"+b);
   led_RED.writeSync(r);
   led_GREEN.writeSync(g);
   led_BLUE.writeSync(b);
 }
 while (true){
+
   setLED(true,false,false);
   sleep.sleep(2); // sleep for ten seconds
   setLED(false,true,false);
