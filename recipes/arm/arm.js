ba88d0dab5ad051c451a8b5ab85d93dba41926da
Leandro David <leandrodvd@gmail.com>
2017-07-16 00:48:20 -0300

    first version

---
+++ recipes/arm/arm.js
@@ -0,0 +1,13 @@
+const TJBot = require('tjbot');
+
+var hardware = ['servo'];
+var configuration = {};
+var credentials = {}
+}
+var tj = new TJBot(hardware, configuration, credentials);
+
+
+function wave(){
+  tj.wave();
+  setTimeout(wave, 5000);
+}
